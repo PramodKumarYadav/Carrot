121bffc001fb948ce2f1bbf6ca2ee2c41641bba3
Pramod Kumar Yadav <p.yadav@payvision.com>
2020-02-18 23:31:29 +0100

    added debug mode

---
+++ TestFramework/debug.ps1
@@ -0,0 +1,15 @@
+
+Start-Transcript -Path "/TestFramework/TestResults/debug.log"
+
+# Install dependent modules from TestModules directory, recursive and forcefully (to get latest changes)
+. '/TestFramework/TestModules/import-modules.ps1'
+Import-Modules -Path '/TestFramework'
+
+Write-Host "In the debug container"
+# Write-Host "Starting test plan '$env:TEST_PLAN' - buildId '$env:BUILD_ID'"
+
+Write-Host "Setup complete. Now going to sleep forever..."
+Stop-Transcript
+
+# Equivalent of CMD tail -f /dev/null
+tail -f /dev/null
